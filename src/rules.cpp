57d00c9fe4045edf1d64936de9c6980e8cbaacbd
Bernardo Sulzbach <mafagafogigante@gmail.com>
2016-11-20 03:45:04 -0200

    Should hint inline at small rules

--- src/rules.cpp
+++ src/rules.cpp
@@ -1,10 +1,6 @@
 #include "rules.hpp"
 #include <string>
 
-bool is_valid_letter(const char letter) {
-  return letter >= 'a' && letter <= 'z';
-}
-
 bool is_valid_word(const std::string &word) {
   for (char letter : word) {
     if (!is_valid_letter(letter)) {
@@ -12,8 +8,4 @@ bool is_valid_word(const std::string &word) {
     }
   }
   return true;
-}
-
-size_t get_letter_index(const char letter) {
-  return static_cast<size_t>(letter - 'a');
 }
--- src/rules.hpp
+++ src/rules.hpp
@@ -8,10 +8,14 @@ const size_t alphabet_size = 'z' - 'a' + 1;
 /**
  * Returns whether or not a letter should be counted.
  */
-bool is_valid_letter(const char letter);
+inline bool is_valid_letter(const char letter) {
+  return letter >= 'a' && letter <= 'z';
+}
+
+inline size_t get_letter_index(const char letter) {
+  return static_cast<size_t>(letter - 'a');
+}
 
 bool is_valid_word(const std::string &word);
-
-size_t get_letter_index(const char letter);
 
 #endif
