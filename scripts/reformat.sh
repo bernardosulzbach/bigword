da881a36d2f23df99dcb4e2a9287974dc6673670
Bernardo Sulzbach <mafagafogigante@gmail.com>
2017-06-03 17:35:41 -0300

    Should have a shebang in the shell scripts
    Also defined COMPILATION_DATABASE.

--- scripts/reformat.sh
+++ scripts/reformat.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
 SECONDS=0
 
+COMPILATION_DATABASE="compile_commands.json"
 # Create the symbolic link we need.
 if [ ! -f $COMPILATION_DATABASE ]; then
   ln -s build/compile_commands.json
